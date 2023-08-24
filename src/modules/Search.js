4136e4ef81ecdb6f68ede82c252149903d692216
Guy Blanchfield <guyblanchfield@gmail.com>
2023-08-25 00:06:54 +0100

    Posts and pages added to search, asynchronous fetches, error handling

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -5,7 +5,7 @@ class Search {
 	constructor() {
 		this.addSearchHTML();
 		// create a post property for the fetched, jsoned result
-		this.posts;
+		// this.posts;
 
 		this.resultsDiv = document.querySelector("#search-overlay__results");
 		// NB! there are two js-search-trigger buttons (one for mobile nav one for desktop nav)
@@ -100,16 +100,54 @@ class Search {
 		// json the response and store it in a property
 		// (created in constructor)
 		// so other methods can use it
-		this.posts = await response.json();
+		// const data = await response.json();
+		// return data;
+		return await response.json();
 	}
 
 	async getResults() {
 		// NB variables are fine here bc they are scoped to a method (getResults)
 		// they would not be allowed in the main scope of the class
 
-		// do the fetch
-		await this.doFetch(universityData.root_url + "/wp-json/wp/v2/posts?search=", this.searchField.value);
+		// do the posts fetch
+		// const posts = await this.doFetch(universityData.root_url + "/wp-json/wp/v2/posts?search=", this.searchField.value);
+		// do the pages fetch
+		// const pages = await this.doFetch(universityData.root_url + "/wp-json/wp/v2/pages?search=", this.searchField.value);
 
+		// true async version (i.e parallel not serial)
+		// brad was using jQuery when().then();
+
+		// nb tried putting .catch() on the end of Promise.all - didn't take
+		try {
+			const [posts, pages] = await Promise.all([
+				this.doFetch(universityData.root_url + "/wp-json/wp/v2/posts?search=", this.searchField.value),
+				this.doFetch(universityData.root_url + "/wp-json/wp/v2/pages?search=", this.searchField.value),
+			]);
+
+			// spread (sources must be arrays):
+			// const combinedResults = [...posts, ...pages]
+			// concat (sources could be anything, should still work)
+			const combinedResults = posts.concat(pages);
+			this.displayResults(combinedResults);
+		} catch (err) {
+			this.clearResults();
+			console.log(`error: ${err}`);
+			const resultsErrorDiv = document.createElement("div");
+			const resultsErrorPara1 = document.createElement("p");
+			const resultsErrorPara2 = document.createElement("p");
+			resultsErrorPara1.textContent = "Sorry, there has been an unexpected error. Please try again.";
+			// resultsErrorPara2.textContent = "Please try again.";
+			resultsErrorDiv.appendChild(resultsErrorPara1);
+			// resultsErrorDiv.appendChild(resultsErrorPara2);
+			this.resultsDiv.insertBefore(resultsErrorDiv, null);
+		}
+	}
+
+	displayResults(results) {
+		// update the results div
+
+		// start by clearing it out (the spinner probably)
+		// and updating the spinner state
 		this.clearResults();
 
 		// the heading
@@ -128,13 +166,13 @@ class Search {
 		this.resultsDiv.insertBefore(resultsHeading, null);
 
 		// this can run unconditionally bc it'll do nothing if there are no results
-		this.posts.forEach((post) => {
+		results.forEach((result) => {
 			// the li
 			const resultsLi = document.createElement("li");
 			// the link
 			const resultsLink = document.createElement("a");
-			resultsLink.href = post.link;
-			const resultsLinkContent = document.createTextNode(post.title.rendered);
+			resultsLink.href = result.link;
+			const resultsLinkContent = document.createTextNode(result.title.rendered);
 			// append link text to link
 			// (the text is a Node so it needs appendChild, the others could use append)
 			// (caniuse says append has 93% support so probably not)
@@ -150,7 +188,7 @@ class Search {
 		// brad did this as ternary bc its all in template literal
 		// in arrow function within jQuery getJSON()
 		// else write 'no information' message
-		if (this.posts.length) {
+		if (results.length) {
 			// try with insertBefore (marginally quicker than appendChild)
 			// (If referenceNode is null, then newNode is inserted at the end of node's child nodes.)
 			this.resultsDiv.insertBefore(resultsList, null);
@@ -174,8 +212,8 @@ class Search {
 		}
 	}
 
+	// this method is a callback for the transitionend eventlistener
 	searchFocus() {
-		// this method is a callback for the transitionend eventlistener
 		this.searchOverlay.addEventListener("transitionend", () => {
 			this.searchField.focus();
 		});
@@ -194,9 +232,11 @@ class Search {
 		// and use that to call the searchfocus method
 		this.searchOverlay.addEventListener("transitionend", this.searchFocus.bind(this));
 
-		// this.searchField.focus();
-		// we're definitely targeting it, but not focusing it
-		// this.searchField.style.visibility = "hidden";
+		// clear the field
+		this.searchField.value = "";
+		// clear the results
+		this.clearResults();
+
 		this.isOverlayOpen = true;
 	}
 
@@ -208,6 +248,9 @@ class Search {
 		// remove the transitionend eventlistener
 		this.searchOverlay.removeEventListener("transitionend", this.searchFocus);
 
+		// remove focus from the search field otherwise it interferes with
+		// the handleKeyPress conditions, which check for inputs or textareas having focus
+		this.searchField.blur();
 		this.isOverlayOpen = false;
 	}
 
